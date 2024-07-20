26b86a16ef1c395f5170af8e5cc4a4a6d7d2d146
Emil Georgiev <emogeorgiev88@gmail.com>
2024-07-20 11:18:55 +0300

    add all comamnds

--- network/p2p/commands.go
+++ network/p2p/commands.go
@@ -3,9 +3,37 @@ package p2p
 import "fmt"
 
 const (
-	cmdPing       = "ping"
-	cmdPong       = "pong"
-	cmdVersion    = "version"
+	cmdPing        = "ping"
+	cmdPong        = "pong"
+	cmdVersion     = "version"
+	cmdVerack      = "verack"
+	cmdSendcmpct   = "sendcmpct"
+	cmdGetheaders  = "getheaders"
+	cmdAddr        = "addr"
+	cmdInv         = "inv"
+	cmdGetdata     = "getdata"
+	cmdNotfound    = "notfound"
+	cmdGetblocks   = "getblocks"
+	cmdTx          = "tx"
+	cmdBlock       = "block"
+	cmdHeaders     = "headers"
+	cmdGetadd      = "getadd"
+	cmdMempoo      = "mempoo"
+	cmdCheckorder  = "checkorder"
+	cmdSubmitorder = "submitorder"
+	cmdReply       = "reply"
+	cmdReject      = "reject"
+	cmdFilterload  = "filterload"
+	cmdFilteradd   = "filteradd"
+	cmdFilterclear = "filterclear"
+	cmdMerkleblock = "merkleblock"
+	cmdAlert       = "alert"
+	cmdSendHeaders = "sendheaders"
+	cmdFeefilter   = "feefilter"
+	cmdCmpctblock  = "cmpctblock"
+	cmdGetblocktxn = "getblocktxn"
+	cmdBlocktxn    = "blocktxn"
+
 	commandLength = 12
 )
 
@@ -26,7 +54,36 @@ const (
 )
 
 var commands = map[string][commandLength]byte{
-	cmdVersion: newCommand(cmdVersion),
+	cmdVersion:     newCommand(cmdVersion),
+	cmdVerack:      newCommand(cmdVerack),
+	cmdPing:        newCommand(cmdPing),
+	cmdPong:        newCommand(cmdPong),
+	cmdSendcmpct:   newCommand(cmdSendcmpct),
+	cmdGetheaders:  newCommand(cmdGetheaders),
+	cmdAddr:        newCommand(),
+	cmdInv:         newCommand(cmdInv),
+	cmdGetdata:     newCommand(cmdGetdata),
+	cmdNotfound:    newCommand(cmdNotfound),
+	cmdGetblocks:   newCommand(cmdGetblocks),
+	cmdTx:          newCommand(cmdTx),
+	cmdBlock:       newCommand(cmdBlock),
+	cmdHeaders:     newCommand(cmdHeaders),
+	cmdGetadd:      newCommand(cmdGetadd),
+	cmdMempoo:      newCommand(cmdMempoo),
+	cmdCheckorder:  newCommand(cmdCheckorder),
+	cmdSubmitorder: newCommand(cmdSubmitorder),
+	cmdReply:       newCommand(cmdReply),
+	cmdReject:      newCommand(cmdReject),
+	cmdFilterload:  newCommand(cmdFilterload),
+	cmdFilteradd:   newCommand(cmdFilteradd),
+	cmdFilterclear: newCommand(cmdFilterclear),
+	cmdMerkleblock: newCommand(cmdMerkleblock),
+	cmdAlert:       newCommand(cmdAlert),
+	cmdSendHeaders: newCommand(cmdSendHeaders),
+	cmdFeefilter:   newCommand(cmdFeefilter),
+	cmdCmpctblock:  newCommand(cmdCmpctblock),
+	cmdGetblocktxn: newCommand(cmdGetblocktxn),
+	cmdBlocktxn:    newCommand(cmdBlocktxn),
 }
 
 func newCommand(command string) [commandLength]byte {
