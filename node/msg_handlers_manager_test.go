982cd3d02ef91a5b0431372573d175b5e72da92a
Emil Georgiev <emogeorgiev88@gmail.com>
2024-07-31 10:30:22 +0300

    stat overview handlers

--- node/msg_handlers_manager_test.go
+++ node/msg_handlers_manager_test.go
@@ -1,44 +1,55 @@
 package node_test
 
-//
-//import (
-//	"github.com/EmilGeorgiev/btc-node/node"
-//	"github.com/golang/mock/gomock"
-//	"testing"
-//)
-//
-//func TestNewMessageHandlersManager_Start(t *testing.T) {
-//	ctrl := gomock.NewController(t)
-//
-//	msgGetHeadersHandler := node.NewMockStartStop(ctrl)
-//	msgGetHeadersHandler.EXPECT().Start().Times(1)
-//	msgGetBlockHandler := node.NewMockStartStop(ctrl)
-//	msgGetBlockHandler.EXPECT().Start().Times(1)
-//	msgPingHandler := node.NewMockStartStop(ctrl)
-//	msgPingHandler.EXPECT().Start().Times(1)
-//
-//	manager := node.NewMessageHandlersManager([]node.StartStop{msgPingHandler, msgGetBlockHandler, msgGetHeadersHandler})
-//
-//	manager.Start()
-//}
-//
-//func TestNewMessageHandlersManager_Stop(t *testing.T) {
-//	ctrl := gomock.NewController(t)
-//
-//	msgGetHeadersHandler := node.NewMockStartStop(ctrl)
-//	msgGetHeadersHandler.EXPECT().Stop().Times(1)
-//	msgGetBlockHandler := node.NewMockStartStop(ctrl)
-//	msgGetBlockHandler.EXPECT().Stop().Times(1)
-//	msgPingHandler := node.NewMockStartStop(ctrl)
-//	msgPingHandler.EXPECT().Stop().Times(1)
-//
-//	manager := node.NewMessageHandlersManager([]node.StartStop{msgPingHandler, msgGetBlockHandler, msgGetHeadersHandler})
-//
-//	manager.Stop()
-//}
-//
-//func TestNewMessageHandlersManager_StartAndStopWithNoHandlers(t *testing.T) {
-//	manager := node.NewMessageHandlersManager([]node.StartStop{})
-//	manager.Start()
-//	manager.Stop()
-//}
+import (
+	"testing"
+
+	"github.com/EmilGeorgiev/btc-node/node"
+	"github.com/golang/mock/gomock"
+)
+
+func TestNewMessageHandlersManager_Start(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	msgBlockHeaders := node.NewMockStartStop(ctrl)
+	msgBlockHeaders.EXPECT().Start().Times(1)
+	msgHeadersHandler := node.NewMockStartStop(ctrl)
+	msgHeadersHandler.EXPECT().Start().Times(1)
+
+	manager := node.NewMessageHandlersManager([]node.StartStop{msgBlockHeaders, msgHeadersHandler}, nil)
+
+	manager.Start()
+}
+
+func TestNewMessageHandlersManager_Stop(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	msgGetHeadersHandler := node.NewMockStartStop(ctrl)
+	msgGetHeadersHandler.EXPECT().Stop().Times(1)
+	msgGetBlockHandler := node.NewMockStartStop(ctrl)
+	msgGetBlockHandler.EXPECT().Stop().Times(1)
+	msgPingHandler := node.NewMockStartStop(ctrl)
+	msgPingHandler.EXPECT().Stop().Times(1)
+
+	manager := node.NewMessageHandlersManager([]node.StartStop{msgPingHandler, msgGetBlockHandler, msgGetHeadersHandler}, nil)
+
+	manager.Stop()
+}
+
+func TestNewMessageHandlersManager_StartAndStopWithNoHandlers(t *testing.T) {
+	manager := node.NewMessageHandlersManager([]node.StartStop{}, nil)
+	manager.Start()
+	manager.Stop()
+}
+
+func TestNewMessageHandlersManager_StartOverviewHandlers(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	msgBlockHeaders := node.NewMockStartStop(ctrl)
+	msgBlockHeaders.EXPECT().Start().Times(0)
+	msgHeadersHandler := node.NewMockStartStop(ctrl)
+	msgHeadersHandler.EXPECT().Start().Times(1)
+
+	manager := node.NewMessageHandlersManager([]node.StartStop{msgBlockHeaders, msgHeadersHandler}, []node.StartStop{msgHeadersHandler})
+
+	manager.StartOverviewHandlers()
+}
